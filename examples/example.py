640dbcb9360b5969e2ee6c37078a378ab0ad5699
hube12 <neilhube12@gmail.com>
2022-07-11 18:30:25 +0200

    Add loop event

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,6 @@
+import subprocess
+import time
+import random
 from rust_py import *
 
 
@@ -10,9 +13,39 @@ logger = PyLogger(log_it)
 logger.configure(PyLogLevel.Info, PyLogTimeFormat.Human, PyLogFormat.Pretty)
 
 runtime = PyRuntime()
-client = PyClient(runtime)
+runner = PyRunner(runtime)
+
+client = runner.client
+subscriber = runner.subscriber
+
+
+def callback_event_1(event):
+    print("Got event 1: {}".format(event))
+    pass
+
+
+def callback_event_2(event):
+    print("Got event 2: {}".format(event))
+    pass
+
+child = subprocess.Popen(['exec', 'bash'], stdout=subprocess.PIPE, shell=True)
+print(child.pid)
+child.kill()
+
+subscriber.subscribe(PyEventType.type_1(), callback_event_1, runtime)
+subscriber.subscribe(PyEventType.type_2(), callback_event_2, runtime)
 
 for i in range(1000):
-    msg = PyMessage.from_string("test")
-    client.send(msg)
-    client.receive()
+    try:
+        if random.choice([True, False]):
+            msg = PyMessage.from_string("test_" + str(i))
+            client.send(msg)
+        else:
+            msg = PyMessage.from_string(str(i))
+            client.send(msg)
+    except Exception as e:
+        print(e)
+
+child = subprocess.Popen(['exec', 'bash'], stdout=subprocess.PIPE, shell=True)
+print(child.pid)
+child.kill()
